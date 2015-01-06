3e6f4738db86956c2dc6ac8af28f77ff315ed467
bonaff <bonaff@live.it>
2015-01-07 00:09:14 +0100

    Aggiunto sorting classe QE. Sempre da testare.
    Fatto il sorting, utilizzando array generici "Comparable".
    Indi per cui il codice è molto sporco e schifido. Se solo potesi usare
    la programmazione generica

--- Esercizi/QE.java
+++ Esercizi/QE.java
@@ -1,3 +1,4 @@
+import java.lang.Comparable;
 public class QE extends Q{
 
 	public QE(){
@@ -7,8 +8,8 @@ public class QE extends Q{
 
 	public void enqueue(Object x){
 
-		if(x instaceof Comparable)
-			super.enqueue(x)
+		if( x instanceof Comparable)
+			super.enqueue(x);
 		else
 			throw new IllegalArgumentException();
 
@@ -22,50 +23,56 @@ public class QE extends Q{
 
 	}
 
-	private Object[] sort(Object[] t){
-
-		if(t.lenght==0)
+	private Object[] sort(Object[] l){
+		Comparable[] t=(Comparable[]) l;
+		if(t.length==0)
 			throw new IllegalArgumentException();
 
-		else if(t.lenght==1)
+		else if(t.length==1)
 			return t;
-		else if(t.lenght==2){
+		else if(t.length==2){
 			if(t[0].compareTo(t[1])>=0)
 				return t;
 			else{
-				Object tmp=t[0];
+				Comparable tmp=t[0];
 				t[0]=t[1];
 				t[1]=tmp;
 				return t;
 			}
 		}
 
-		Object[] t1=t.lenght/2;
-		Object[] t2=t.lenght/2+1;
+		Comparable[] t1=new Comparable[t.length/2];
+		Comparable[] t2=new Comparable[t.length/2+1];
 
 		//divido in due l'array. Mettendo un elemento in più nel secondo nel caso l'array di partenza sia dispari
-		for(int i=0;i<t.lenght;i++){
+		for(int i=0;i<t.length;i++){
 
-			if(i<t.lenght/2)
+			if(i<t.length/2)
 				t1[i]=t[i];
 
 			else{
-				t2[i-lenght/2]=t[i];
+				t2[t.length/2]=t[i];
 			}
 
 		}
 
 		//ordino i due array
-		t1=sort(t1);
-		t2=sort(t2);
+		t1=(Comparable[])sort(t1);
+		t2=(Comparable[])sort(t2);
 
 		//i=index array principale
 		//i1 index array 1 (t1)
 		//i2 index array 2 (t2)
 
-		for(int i=0, i1=0, i2=0;i<t.lenght;i++){
+		for(int i=0, i1=0, i2=0;i<t.length;i++){
 
-			if(i1>=t1.lenght){}
+			if(i1>=t1.length){
+				t[i]=t2[i2];
+				i2++;
+			}
+			else if(i2>=t2.length){
+
+			}
 
 			if(t1[i1].compareTo(t2[i2]) >= 0){
 				t[i]=t1[i1];
@@ -76,11 +83,9 @@ public class QE extends Q{
 				i2++;
 			}
 		}
-
-				
-
-
+		return t;
 	}
+		
 
 
 }
